d04e8e5ba17de1a3e37e162165e900393cf9d8fb
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 18:16:50 +0000

    Add shop db schema to shop model

--- server/models/shop.js
+++ server/models/shop.js
@@ -1,2 +1,18 @@
 const mongoose = require('mongoose');
 const config = require('../config/database');
+
+//Shop Schema
+
+const ShopSchema = mongoose.Schema({
+	_id: {type: mongoose.Schema.Types.ObjectId},
+	picture: {type: String},
+	name: {type: String},
+	email: {type: String},
+	city: {type: String},
+	location: {
+		type: {type: String},
+		coordinates: {type: Array}
+	}
+});
+
+const Shop = module.exports = mongoose.model('shop', ShopSchema, "shops");
