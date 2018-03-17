52157ffa64ae12eb03c741aff08b84212bf580ba
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 16:43:46 +0000

    Add Express server entry file

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// Port Configuration
+const port = process.env.PORT || 1337
+
+// Start server
+app.listen(port, () => {
+	console.log('Server started on Port: ' + port);
+});
