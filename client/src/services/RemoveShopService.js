ff034ea88fc5f9bcfd49c7cb553f7089b42f2989
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 21:48:57 +0000

    Add RemoveShopService to Vue app

---
+++ client/src/services/RemoveShopService.js
@@ -0,0 +1,18 @@
+import Api from '@/services/Api'
+import axios from 'axios'
+import store from '@/store/store'
+
+function addToken () {
+  const token = store.state.token
+  if (token) {
+    axios.defaults.headers.common['Authorization'] = token
+  } else {
+    axios.defaults.headers.common['Authorization'] = null
+  }
+}
+export default (shopID) => {
+  addToken()
+  const userID = store.state.user.id
+  const credentials = {shopID: shopID, userID: userID}
+  return Api().post('api/removeShop', credentials)
+}
