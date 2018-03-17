f0f99afa86c2c75ba6b5baf8ebcf277353323088
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 18:12:29 +0000

    Add shops route file to Express Server

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ mongoose.connection.on('error', (error) =>{
 const app = express();
 
 const users = require('./routes/users');
+const shops = require('./routes/shops');
 
 // Port Configuration
 const port = process.env.PORT || 1337
@@ -44,6 +45,7 @@ app.use(passport.session());
 require('./config/passport')(passport)
 
 app.use('/users', users);
+app.use('/api', shops);
 
 //Routing
 app.get('/', (req, res) => {
