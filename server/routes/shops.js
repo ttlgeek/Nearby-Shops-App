f4365cf6c15e6c765f032374ae64a6eaff063d8c
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 18:22:21 +0000

    Expose NearbyShops endpoint equipped with route protection using passport for Express server

--- server/routes/shops.js
+++ server/routes/shops.js
@@ -1,2 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const Shop = require('../models/shop');
+
+// Return User's Nearby Shops
+
+router.get('/nearbyShops', passport.authenticate('jwt', {session: false}),(req, res, next) => {
+	const query = {
+		location: {
+			$near: {
+				$geometry: {
+					type: "Point",
+					coordinates: [ 0 , 0 ]
+				}
+			}
+		}
+	}
+
+	Shop.find(query, (err, shops) => {
+		if (err) throw err;
+		return res.json(shops);
+	})
+});
+
+module.exports = router;
