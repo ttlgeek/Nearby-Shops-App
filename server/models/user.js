8c98dfbd3ef3e9151399ee119a6b8862477f736e
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 17:26:55 +0000

    Add getUserById function to User model

--- server/models/user.js
+++ server/models/user.js
@@ -19,3 +19,7 @@ const UserSchema = mongoose.Schema({
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = (id, callback) => {
+	User.findById(id, callback);
+}
