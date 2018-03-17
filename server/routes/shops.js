31513665e2102f9e0f417c1c785502df1663e5c0
Mohamed El Mahdali <mohamedelmahdali11@gmail.com>
2018-03-17 18:26:15 +0000

    Expose PreferredShops endpoint equipped with route protection using passport for Express server

--- server/routes/shops.js
+++ server/routes/shops.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const Shop = require('../models/shop');
+const User = require('../models/user');
 
 // Return User's Nearby Shops
 
@@ -21,6 +22,15 @@ router.get('/nearbyShops', passport.authenticate('jwt', {session: false}),(req, 
 		if (err) throw err;
 		return res.json(shops);
 	})
+});
+
+// Return User's Preferred Shops
+
+router.post('/preferredShops', passport.authenticate('jwt', {session: false}), (req, res, next) => {
+    User.getUserByEmail(req.body.email, (err, user) => {
+    	if (err) throw err;
+    	return res.json(user.preferred)
+    });
 });
 
 module.exports = router;
